9f35c357ed868fd21152970efc272e859ef4929f
Admin <Admin@192.168.1.26>
2020-09-08 10:36:34 +0700

    version1

---
+++ src/main/java/com/mycompany/github/Student.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.github;
+
+
+public class Student {
+   private int studentId; 
+   private String studentName;
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+   
+}
